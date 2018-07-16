afcbaf279b659a7288e15ea1bf0ef2196535d1a0
Dan Thompson <dthompson@vestmark.com>
2018-07-16 16:08:22 -0400

    login tests

--- app/Components/Login/Login.test.js
+++ app/Components/Login/Login.test.js
@@ -1,16 +1,45 @@
 import React from 'react';
 import { shallow } from 'enzyme';
-import { stub } from 'sinon';
+import { fake, stub } from 'sinon';
 import Service from 'Business/Auth/Service';
 
 import Login from './Login';
 
 describe('#Components #Login #Login', function() {
+    const authServiceSpy = fake.resolves('12345');
+
     beforeAll(function() {
-        stub(Service, 'get').callsFake(() => undefined);
+        stub(Service, 'get').callsFake(authServiceSpy);
     });
 
     it('should render.', function() {
         expect(() => shallow(<Login />)).not.toThrow();
+    });
+
+    it('should have an error message if the user tries to login without a username.', function() {
+        const wrapper = shallow(<Login />);
+
+        wrapper.setState({password: 'test'});
+        wrapper.find('input[type="button"]').simulate('click');
+
+        expect(wrapper.state()).toHaveProperty('usernameError', 'required');
+    });
+
+    it('should have an error message if the user tries to login without a password.', function() {
+        const wrapper = shallow(<Login />);
+
+        wrapper.setState({username: 'test'});
+        wrapper.find('input[type="button"]').simulate('click');
+
+        expect(wrapper.state()).toHaveProperty('passwordError', 'required');
+    });
+
+    it('should request an auth token.', function() {
+        const wrapper = shallow(<Login />);
+
+        wrapper.setState({username: 'testUsername', password: 'testPassword'});
+        wrapper.find('input[type="button"]').simulate('click');
+
+        expect(authServiceSpy.calledWith('testUsername', 'testPassword')).toBe(true);
     });
 });
