b08abd4bd26e1a6ad36c9d1b930bd1aa88b3fadc
Dan Thompson <dthompson@vestmark.com>
2018-07-20 11:27:28 -0400

    make it easy to go from Message to InputMessage

--- app/UI/Forms/Validation/InputMessage.js
+++ app/UI/Forms/Validation/InputMessage.js
@@ -1,7 +1,12 @@
 import isError from 'Business/Validation/isError';
 
 //Business.Validation.Types.ErrorLevel => String => {}
-function InputMessage(type, message) {
+function InputMessage(m) {
+    if (!m) {
+        throw new TypeError('Provide a message.');
+    }
+    const { type, message } = m;
+
     if (!isError(type)) {
         throw new TypeError('Type must be a valid error type.');
     }
--- app/UI/Forms/Validation/InputMessage.test.js
+++ app/UI/Forms/Validation/InputMessage.test.js
@@ -1,16 +1,31 @@
 import ErrorLevel from 'Business/Validation/Types/ErrorLevel';
+import Message from 'Business/Validation/Types/Message';
 import InputMessage from 'DinDin/UI/Forms/Validation/InputMessage';
 
 
 describe('#UI #Forms #Validation #InputMessage', function() {
     it('should return an object with an error type and message.', function() {
-        const result = new InputMessage(ErrorLevel.error, 'You messed up!');
+        const result = new InputMessage({type: ErrorLevel.error, message: 'You messed up!'});
         const expected = {
             message: 'You messed up!',
             errorLevel: ErrorLevel.error
         };
 
         expect(result).toEqual(expected);
+    });
+
+    it('should accept a Business.Validation.Types.Message.', function() {
+        const messsage = new Message(ErrorLevel.error, 'some field', 5, 'You messed up!');
+        const expected = {
+            message: 'You messed up!',
+            errorLevel: ErrorLevel.error
+        };
+
+        expect(new InputMessage(messsage)).toEqual(expected);
+    });
+
+    it('should throw a type error if no argument is provided.', function() {
+        expect(() => new InputMessage()).toThrow('Provide a message.');
     });
 
     it('should throw a type error if a valid error level is not provided', function() {
