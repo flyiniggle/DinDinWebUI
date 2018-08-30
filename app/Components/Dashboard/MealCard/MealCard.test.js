ab54a802aaeaf7afdb715e1956e4edb2ba680c67
Dan Thompson <d.thompso@yahoo.com>
2018-08-29 21:23:18 -0400

    tests for meal card

---
+++ app/Components/Dashboard/MealCard/MealCard.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+
+import MealCard from './MealCard';
+
+describe('#Components #Dashboard #MealCard', function() {
+    const meal = {
+        id: 1,
+        name: 'Delicious Meal',
+        owner: '1',
+        taste: 5,
+        difficulty: 3,
+        lastUsed: '2018-05-12',
+        usedCount: 3,
+        notes: 'this is a really good meal'
+    };
+
+    it('should render.', function() {
+        expect(shallow(<MealCard meal={ meal } />)).toMatchSnapshot();
+    });
+
+    it('should show the meal name.', function() {
+        const wrapper = shallow(<MealCard meal={ meal } />);
+
+        expect(/Delicious Meal/.test(wrapper.text()));
+    });
+
+    it('should show the date the meal was last used.', function() {
+        const wrapper = shallow(<MealCard meal={ meal } />);
+
+        expect(wrapper.find('.lastUsed').text()).toEqual('May 11, 2018');
+    });
+
+    it('should show the meal taste rating.', function() {
+        const wrapper = shallow(<MealCard meal={ meal } />);
+
+        expect(wrapper.find('.taste').text()).toEqual('5');
+    });
+
+    it('should show the difficulty rating.', function() {
+        const wrapper = shallow(<MealCard meal={ meal } />);
+
+        expect(wrapper.find('.difficulty').text()).toEqual('3');
+    });
+
+    it('should show the numbe of times the meal was used.', function() {
+        expect(shallow(<MealCard meal={ meal } />).find('.usedCount').text()).toEqual('Used 3 times');
+
+        meal.usedCount = 1;
+        expect(shallow(<MealCard meal={ meal } />).find('.usedCount').text()).toEqual('Used 1 time');
+
+        meal.usedCount = 0;
+        expect(shallow(<MealCard meal={ meal } />).find('.usedCount').text()).toEqual('Used 0 times');
+    });
+});
--- app/Components/Dashboard/MealCard/MealCard.tsx
+++ app/Components/Dashboard/MealCard/MealCard.tsx
@@ -14,15 +14,15 @@ function MealCard({ meal }: MealCardProps) {
         <Link to={`/meals/${meal.id}`} className="mealCard row p-lg-4 p-2 mb-2">
             <div className="col-4">
                 <h2>{meal.name}</h2>
-                <h4>Last Used: </h4><span>{dateString.display(meal.lastUsed)}</span>
+                <h4>Last Used: </h4><span className='lastUsed'>{dateString.display(meal.lastUsed)}</span>
             </div>
             <div className="col-4">
-                <div><h4 className="d-inline">Taste: </h4><span>{meal.taste}</span></div>
-                <div><h4 className="d-inline">Difficulty: </h4><span>{meal.difficulty}</span></div>
+                <div><h4 className="d-inline">Taste: </h4><span className="taste">{meal.taste}</span></div>
+                <div><h4 className="d-inline">Difficulty: </h4><span className="difficulty">{meal.difficulty}</span></div>
             </div>
             <div className="col-4">
                 <button className="btn btn-primary" type="button">Use it!</button>
-                <h4>Used {meal.usedCount} {(meal.usedCount === 1) ? 'time' : 'times'}</h4>
+                <h4 className="usedCount">Used {meal.usedCount} {(meal.usedCount === 1) ? 'time' : 'times'}</h4>
             </div>
 
         </Link>
