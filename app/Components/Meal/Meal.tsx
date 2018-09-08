bc0668f8cfc8cbb0e60fed655d0af0d70db5a4d7
Dan Thompson <d.thompso@yahoo.com>
2018-09-07 21:40:17 -0400

    some stylin'

--- app/Components/Meal/Meal.tsx
+++ app/Components/Meal/Meal.tsx
@@ -1,13 +1,16 @@
 import * as React from 'react';
+import { Link } from 'react-router-dom';
 import { ReactComponentProps } from 'react-router-dom';
 import getMealById from 'Business/Meals/getMealById';
 import Header from 'Components/Header/Header';
 import TextInput from 'UI/Forms/TextInput/TextInput';
 import dateString from 'UI/Formatting/dateString';
-import Meal from 'Business/Meals/Types/Meal';
+import IMeal from 'Business/Meals/Types/Meal';
+
+import './Meal.sass';
 
 interface MealProps {
-    meals?: Meal[],
+    meals?: IMeal[],
     logoutHandler: () => void,
     match: ReactComponentProps,
     useMeal: (Meal) => Promise<void>
@@ -17,24 +20,40 @@ function Meal(props: MealProps) {
     if (!props.meals) {
         return <p>Loading...</p>
     }
-    const meal = getMealById(props.match.params.id, props.meals);
+    const meal: IMeal = getMealById(props.match.params.id, props.meals);
 
     return (
-        <div className="mainBackground container-fluid dashboard">
+        <div className="meal mainBackground container-fluid">
             <div className="row">
                 <div className="col-12">
                     <Header logoutHandler={props.logoutHandler} />
                 </div>
             </div>
-            <div className="row">
-                <div className="col-12 col-lg-6">
-                    <h2>
-                        {meal.name}</h2>
+            <div className="row m-2">
+                <h1>{meal.name}</h1>
+            </div>
+            <div className="row m-2">
+                <div className="col-12 col-lg-2">
+                    <h4>Ingredients</h4>
+                </div>
+                <div className="col-12 col-lg-5">
                     <div><h4 className="d-inline">Taste: </h4><span className="taste">{meal.taste}</span></div>
                     <div><h4 className="d-inline">Difficulty: </h4><span className="difficulty">{meal.difficulty}</span></div>
                 </div>
-                <div className="col-12 col-lg-6">
-                    <h4>Last Used: </h4><span className='lastUsed'>{dateString.display(meal.lastUsed)}</span>
+                <div className="col-12 col-lg-5">
+                    <div><h4 className="d-inline">Last Used: </h4><span className='lastUsed'>{dateString.display(meal.lastUsed)}</span></div>
+
+                    <h4 className="usedCount">Used {meal.usedCount} {(meal.usedCount === 1) ? 'time' : 'times'}</h4>
+                </div>
+            </div>
+            <div className="row m-2">
+                <div className="col-12">
+                    <h4>Notes:</h4>
+                    <span>{meal.notes}</span>
+                </div>
+            </div>
+            <div className="row m2">
+                <div className="col-2 d-flex justify-content-between">
                     <button
                         className="btn btn-primary"
                         type="button"
@@ -43,13 +62,8 @@ function Meal(props: MealProps) {
                             event.preventDefault();
                             return props.useMeal(meal);
                         }}>Use it!</button>
-                    <h4 className="usedCount">Used {meal.usedCount} {(meal.usedCount === 1) ? 'time' : 'times'}</h4>
-                </div>
-            </div>
-            <div className="row">
-                <div className="col-12">
-                    <h4>Notes:</h4>
-                    <span>{meal.notes}</span>
+
+                    <Link to="/dashboard" className="btn btn-outline-primary">close</Link>
                 </div>
             </div>
         </div>
