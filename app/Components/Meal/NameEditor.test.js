dd4c22af522d778059e703a8609e2cae83b3d361
Dan Thompson <d.thompso@yahoo.com>
2018-09-13 22:54:21 -0400

    just a few tests for  name editor

---
+++ app/Components/Meal/NameEditor.test.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { mount, shallow } from 'enzyme';
+import NameEditor from 'Components/Meal/NameEditor';
+
+
+describe('#Components #Meal #NameEditor', function() {
+    it('should call the onSave handler when the user clicks save.', function() {
+        const props = {
+            name: 'test meal',
+            onChange: jest.fn(),
+            onSave: jest.fn(),
+            onCancel: jest.fn()
+        };
+        const wrapper = shallow(<NameEditor { ...props } />);
+
+        wrapper.find('.name-editor-save').simulate('click');
+
+        expect(props.onSave).toHaveBeenCalled();
+    });
+
+    it('should call the onCancel handler when the user clicks cancel.', function() {
+        const props = {
+            name: 'test meal',
+            onChange: jest.fn(),
+            onSave: jest.fn(),
+            onCancel: jest.fn()
+        };
+        const wrapper = shallow(<NameEditor { ...props } />);
+
+        wrapper.find('.name-editor-cancel').simulate('click');
+
+        expect(props.onCancel).toHaveBeenCalled();
+    });
+
+    it('should show the name in the text input.', function() {
+        const props = {
+            name: 'test meal',
+            onChange: jest.fn(),
+            onSave: jest.fn(),
+            onCancel: jest.fn()
+        };
+        const wrapper = mount(<NameEditor { ...props } />);
+
+
+        //expect(wrapper.find('input[type="text"]').attr('value')).toEqual(props.name);
+    });
+});
---
+++ app/Components/Meal/NameEditor.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import TextInput from 'UI/Forms/TextInput/TextInput';
+import AsyncButton from 'UI/Forms/AsyncButton/AsyncButton';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBan, faCheck } from '@fortawesome/free-solid-svg-icons';
+
+
+interface NameEditorProps {
+    name: string,
+    onChange: (e: React.FormEvent) => void,
+    onSave: () => void,
+    onCancel: (e: Event) => void
+}
+
+function NameEditor(props: NameEditorProps) {
+    return (
+        <div className="input-group">
+            <TextInput value={props.name} className="form-control-lg" onChange={props.onChange} />
+            <div className="input-group-append">
+                <AsyncButton onClick={props.onSave} className="name-editor-save btn btn-lg btn-primary">
+                    <FontAwesomeIcon icon={faCheck} />
+                </AsyncButton>
+                <AsyncButton onClick={props.onCancel} className="name-editor-cancel btn btn-lg btn-outline-primary">
+                    <FontAwesomeIcon icon={faBan} />
+                </AsyncButton>
+            </div>
+        </div>
+    );
+
+}
+
+export default NameEditor
