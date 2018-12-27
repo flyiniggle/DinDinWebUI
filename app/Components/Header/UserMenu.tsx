b3f96720a6b43d2e389715b486a37c4c2e500a73
flyiniggle <d.thompso@yahoo.com>
2018-12-27 08:34:20 -0500

    leave user menu reading from local storage for now

--- app/Components/Header/UserMenu.tsx
+++ app/Components/Header/UserMenu.tsx
@@ -1,39 +1,28 @@
+import UserContext from 'Business/Auth/UserContext';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faUserCircle } from '@fortawesome/free-solid-svg-icons';
 import * as React from 'react';
-import { connect } from 'react-redux'
-import { Maybe } from 'true-myth';
-
-import { username, email } from "Data/Selectors/userSelectors";
 
 interface UserMenuProps {
     logoutHandler: () => void
-    username: Maybe<string>
-    email: Maybe<string>
 }
-function UserMenuBase(props: UserMenuProps) {
-    const { username } = props;
-
+function UserMenu(props: UserMenuProps) {
     return (
-            <div className="d-flex align-items-baseline">
-            <h2 className="d-inline"><FontAwesomeIcon icon={faUserCircle} /> {username.unwrapOr('not logged in')}</h2>
-                <button
-                    type="button"
-                    onClick={props.logoutHandler}
-                    className="btn btn-sm btn-outline-primary">
-                    Log out
-                </button>
-            </div>
+        <UserContext.Consumer>
+            {username => (
+                <div className="d-flex align-items-baseline">
+                    <h2 className="d-inline"><FontAwesomeIcon icon={faUserCircle} /> {username}</h2>
+                    <button
+                        type="button"
+                        onClick={props.logoutHandler}
+                        className="btn btn-sm btn-outline-primary">
+                        Log out
+                    </button>
+                </div>
+            )
+            }
+        </UserContext.Consumer>
     );
 }
-
-const mapStateToProps = function(state) {
-    return {
-        username: username(state),
-        email: email(state)
-    }
-}
-
-const UserMenu = connect(mapStateToProps, {})(UserMenuBase);
 
 export default UserMenu;
