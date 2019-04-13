a2564cc443a6b7631c77c4f81446f0882dc3a995
flyiniggle <d.thompso@yahoo.com>
2019-04-13 16:12:14 -0400

    ios in pwa

--- webpack.config.js
+++ webpack.config.js
@@ -116,19 +116,23 @@ module.exports = {
             }, {
                 src: path.join(__dirname, 'manifest', '96.png'),
                 sizes: '96x96',
-                type: 'image/png'
+                type: 'image/png',
+                ios: true
             }, {
                 src: path.join(__dirname, 'manifest', '128.png'),
                 sizes: '128x128',
-                type: 'image/png'
+                type: 'image/png',
+                ios: true
             }, {
                 src: path.join(__dirname, 'manifest', '144.png'),
                 sizes: '144x144',
-                type: 'image/png'
+                type: 'image/png',
+                ios: true
             }, {
                 src: path.join(__dirname, 'manifest', '192.png'),
                 sizes: '192x192',
-                type: 'image/png'
+                type: 'image/png',
+                ios: true
             }, {
                 src: path.join(__dirname, 'manifest', '512.png'),
                 sizes: '512x512',
