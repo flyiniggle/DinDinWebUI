37aac41f89b261b948df0e768ea2cc2bbe6a4da9
Dan Thompson <d.thompso@yahoo.com>
2019-04-18 19:31:58 -0400

    remove logging

--- app/Business/Validation/Lib/getMessageById.ts
+++ app/Business/Validation/Lib/getMessageById.ts
@@ -3,7 +3,6 @@ import Message from '../Types/Message';
 import { Maybe } from 'true-myth';
 
 function getMessageById(id: string, messages: Message[]): Maybe<Message> {
-    console.log(messages.find(propEq('id', id)))
     return Maybe.find(propEq('id', id), messages)
 } 
 
