ff535fac296bb11d029bb950ad4dfc4a36a0259b
flyiniggle <d.thompso@yahoo.com>
2019-04-20 12:43:04 -0400

    new icon and fonts

--- app/Components/Splash/Splash.js
+++ app/Components/Splash/Splash.js
@@ -1,16 +1,17 @@
 import SignUp from 'Components/SignUp/SignUp';
 import React from 'react';
-import { MobileScreen, DesktopScreen } from 'react-responsive-redux';
+import { PhoneScreen, TabletScreen, DesktopScreen } from 'react-responsive-redux';
 import { Link, Switch, Route } from 'react-router-dom';
 import Login from 'Components/Login/Login';
 
-import logo from './Assets/splash-logo-temp.svg';
+import SplashIcon from './SplashIcon';
+
 import './Splash.sass';
 
 
 function Splash() {
     return (
-        <div className="background">
+        <div className="background overflow-hidden">
             <div className="container-fluid">
                 <div className="splash">
                     <div className="row splash-header d-flex flex-row-reverse">
@@ -27,12 +28,27 @@ function Splash() {
                         <Route exact
                             path="/"
                             render={ () => (
-                                <div className="row d-flex justify-content-center align-items-center">
-                                    <MobileScreen className="w-75">
-                                        <img src={ logo } alt="logo" className="mt-5 w-100" />
-                                    </MobileScreen>
-                                    <DesktopScreen className="w-25">
-                                        <img src={ logo } alt="logo" className="mt-5 w-100" />
+                                <div className="row d-flex justify-content-center p-5">
+                                    <PhoneScreen className="d-flex logo-direction align-items-center">
+                                        <SplashIcon
+                                            width={ 150 }
+                                            height={ 150 }
+                                            className="splash-icon" />
+                                        <h1 className="splash-title phone">DinDin</h1>
+                                    </PhoneScreen>
+                                    <TabletScreen className="d-flex logo-direction align-items-center">
+                                        <SplashIcon
+                                            width={ 175 }
+                                            height={ 175 }
+                                            className="splash-icon" />
+                                        <h1 className="splash-title tablet">DinDin</h1>
+                                    </TabletScreen>
+                                    <DesktopScreen className="d-flex flex-column align-items-center">
+                                        <SplashIcon
+                                            width={ 250 }
+                                            height={ 250 }
+                                            className="splash-icon" />
+                                        <h1 className="splash-title desktop">DinDin</h1>
                                     </DesktopScreen>
                                 </div>
                             ) } />
