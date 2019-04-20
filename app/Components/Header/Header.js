32dd18b069e85bbea66aaf0a05d57f8c160222a4
flyiniggle <d.thompso@yahoo.com>
2019-04-20 13:00:04 -0400

    fonts

--- app/Components/Header/Header.js
+++ app/Components/Header/Header.js
@@ -7,11 +7,11 @@ function Header(props) {
     return (
         <div className="headerContainer ">
             <div className="header position-fixed row d-flex justify-content-between">
-                <div className="col-6">
+                <div className="col-6 logo">
                     <h2>Din Din</h2>
                 </div>
                 <div className="col-6">
-                    <UserMenu { ...props } />
+                    <UserMenu {...props} />
                 </div>
             </div>
         </div>
