7f5e7a37de02b91b1070a9053ae14cf64d6de4af
flyiniggle <d.thompso@yahoo.com>
2019-05-01 17:15:17 -0300

    minor layout fix

--- app/Components/Header/Header.js
+++ app/Components/Header/Header.js
@@ -6,11 +6,11 @@ import './Header.sass';
 function Header(props) {
     return (
         <div className="header row d-flex justify-content-between">
-            <div className="col-6 logo">
+            <div className="col-6 pb-1 logo">
                 <h2>Din Din</h2>
             </div>
-            <div className="col-6">
-                <UserMenu {...props} />
+            <div className="col-6 pb-1 ">
+                <UserMenu { ...props } />
             </div>
         </div>
     );
